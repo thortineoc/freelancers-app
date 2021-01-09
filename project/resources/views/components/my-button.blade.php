0122066d1d2ab5949537b9bc4bdd7233d9f90436
Angelika Kubicka <angkubicka99@gmail.com>
2021-01-09 11:58:33 +0100

    My button component

--- project/resources/views/components/my-button.blade.php
+++ project/resources/views/components/my-button.blade.php
@@ -1,3 +1,3 @@
-<div class="rounded-lg shadow text-white bg-green-900 max-w-40">
-    <button type="submit">{{ $slot }}</button>
-</div>
+<button type="submit" class="rounded-lg shadow text-white bg-green-900 max-w-40 block py-2 px-3 text-center hover:text-yellow-300">
+    {{ $slot }}
+</button>
--- project/resources/views/myorders/create.blade.php
+++ project/resources/views/myorders/create.blade.php
@@ -3,7 +3,7 @@
     <x-slot name="header">
         <div class="flex justify-between">
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                {{ __('Creating a new offer') }}
+                {{ __('Creating a new order') }}
             </h2>
         </div>
     </x-slot>
@@ -24,21 +24,21 @@
             <form class="m-3" method="post" action="/dashboard">
                 @csrf
 
-                <div>
-                    <x-label for="isbn" :value="__('Job type')" />
-                    <x-input id="isbn" class="block mt-1 w-full" type="text" name="isbn" :value="old('isbn')" required autofocus />
-                </div>
-                <div>
-                    <x-label for="title" :value="__('Description')" />
+                <div class="my-3">
+                    <x-label for="title" :value="__('Job type')" />
                     <x-input id="title" class="block mt-1 w-full" type="text" name="title" :value="old('title')" required autofocus />
                 </div>
-                <div>
-                    <x-label for="description" :value="__('Payment')" />
-                    <x-input id="description" class="block mt-1 w-full" type="text" name="description" :value="old('description')" required autofocus />
+                <div class="my-3">
+                    <x-label for="description" :value="__('Description')" />
+                    <textarea id=description" class="w-full px-3 py-2 text-gray-700 border rounded-lg focus:outline-none" rows="4"></textarea>
                 </div>
-                <div>
-                    <x-label for="description" :value="__('Deadline')" />
-                    <x-input id="description" class="block mt-1 w-full" type="text" name="description" :value="old('description')" required autofocus />
+                <div class="my-3">
+                    <x-label for="payment" :value="__('Payment')" />
+                    <x-input id="payment" class="block mt-1 w-full" type="text" name="payment" :value="old('payment')" required autofocus />
+                </div>
+                <div class="mb-8 mt-3">
+                    <x-label for="deadline" :value="__('Deadline')" />
+                    <x-input id="deadline" class="block mt-1 w-full" type="text" name="deadline" :value="old('deadline')" required autofocus />
                 </div>
 
                 <x-my-button>Create</x-my-button>
