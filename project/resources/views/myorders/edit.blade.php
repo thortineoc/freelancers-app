329a2c4e80758677c2c30c92b2ee893d678cd800
Angelika Kubicka <angkubicka99@gmail.com>
2021-01-09 23:38:54 +0100

    Editing orders

---
+++ project/resources/views/myorders/edit.blade.php
@@ -0,0 +1,51 @@
+<x-app-layout>
+
+    <x-slot name="header">
+        <div class="flex justify-between">
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                {{ __('Editing the order') }}
+            </h2>
+        </div>
+    </x-slot>
+
+    <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
+
+        @if ($errors->any())
+            <div>
+                <ul class="mt-3 list-disc list-inside text-sm text-red-600">
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+
+        <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+            <form class="m-3" method="post" action="{{ route('myorders.update', $order) }}">
+                @csrf
+                @method("PUT")
+
+                <div class="my-3">
+                    <x-label for="title" :value="__('Job type')" class="font-semibold" />
+                    <x-input id="title" class="block mt-1 w-full" type="text" name="title" :value="$order->title" required autofocus />
+                </div>
+                <div class="my-3">
+                    <x-label for="description" :value="__('Description')" />
+                    <textarea id="description" name="description" class="w-full rounded-md shadow-sm border-gray-300 focus:border-green-100 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" rows="4">{{$order->description}}</textarea>
+                </div>
+                <div class="my-3">
+                    <x-label for="payment" :value="__('Payment')" />
+                    <x-input id="payment" class="block mt-1 w-full" type="number" name="payment" placeholder="e.g. 799,99" :value="$order->budget" required autofocus />
+                </div>
+                <div class="mb-8 mt-3">
+                    <x-label for="deadline" :value="__('Deadline')" />
+                    <x-input id="deadline" class="block mt-1 w-full" type="text" name="deadline" :value="$order->deadline" required autofocus />
+                </div>
+
+                <x-my-button>Update</x-my-button>
+
+            </form>
+        </div>
+
+    </div>
+</x-app-layout>
--- project/resources/views/myorders/index.blade.php
+++ project/resources/views/myorders/index.blade.php
@@ -33,12 +33,14 @@
                         <a href="\dashboard">See offers...</a>
                     </div>
                     <div class="flex">
-                        <button>
-                            <svg class="m-2 h-6 w-6 text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
-                            </svg>
-                        </button>
 
+                        <form method="get" action="{{ route('myorders.edit', $order) }}">
+                            <button>
+                                <svg class="m-2 h-6 w-6 text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
+                                </svg>
+                            </button>
+                        </form>
 
                         <form method="post" action="{{route('myorders.destroy', $order)}}">
 
