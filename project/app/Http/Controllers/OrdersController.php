3f14daf1be89ab11fbfe5056abebe0718dd3ee1d
Michal Stadryniak <stadryniak@student.agh.edu.pl>
2021-01-11 23:27:29 +0100

    get instead of crud

--- project/app/Http/Controllers/OrdersController.php
+++ project/app/Http/Controllers/OrdersController.php
@@ -3,88 +3,19 @@
 namespace App\Http\Controllers;
 
 use App\Models\Order;
-use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
 
 class OrdersController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * Display all non outdated orders without accepted offers
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function __invoke()
     {
-        $orders = Order::all();
-
+        // > is correct operator in this case
+        $orders = Order::where('deadline', '>', Carbon::now())->get();
         return view('orders.index')->withOrders($orders);
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        echo "OrdersController create";
-
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        echo "OrdersController show";
-
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        echo "OrdersController edit";
-
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
     }
 }
