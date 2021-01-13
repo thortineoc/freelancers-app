b984782bfc42af233bd529e9ab2bbdaa77b482a5
Elżbieta Kubicka <ekubicka@student.agh.edu.pl>
2021-01-13 14:24:43 +0100

    Updated OrdersCept

--- project/tests_codeception/acceptance/OrdersCept.php
+++ project/tests_codeception/acceptance/OrdersCept.php
@@ -1,26 +1,26 @@
 <?php
 $I = new AcceptanceTester($scenario);
 $I->wantTo('browse and create orders');
-/*
+
 $I->amOnPage('/orders');
 
 $I->seeCurrentUrlEquals('/login');
 
 $I->fillField('email', 'john.doe@gmail.com');
 $I->fillField('password', 'secret');
-$I->click('Login');*/
+$I->click('Login');
 
 //--------use below part until middleware is added
-$I->amOnPage('/login');
+/*$I->amOnPage('/login');
 $I->fillField('email', 'john.doe@gmail.com');
 $I->fillField('password', 'secret');
 $I->click('Login');
-$I->amOnPage('/orders');
+$I->amOnPage('/orders');*/
 //-------------------------------------------------------
 
 $I->seeCurrentUrlEquals('/orders');
 
-$I->see('new_order1');
+$I->see('No job offer is currently available');
 $I->seeLink('Create new...', '/myorders/create');
 $I->click('Create new...');
 
@@ -29,23 +29,24 @@ $I->see('Creating a new order');
 $I->click('Create');
 
 $I->seeCurrentUrlEquals('/myorders/create');
-$I->fillField('title', 'Grafik');
-$I->click('Create');
-
-$I->seeCurrentUrlEquals('/myorders/create');
-$I->see('Creating a new order');
-$I->seeInField('title', 'Grafik');
-$I->fillField('payment', 'Ethernal gratitude');
+$I->fillField('title', 'DevOps');
 $I->click('Create');
 
 $I->seeCurrentUrlEquals('/myorders/create');
 $I->see('Creating a new order');
 $I->seeInField('title', 'DevOps');
-$I->seeInField('payment', '10');
+$I->fillField('payment', 'Eternal gratitude');
+$I->click('Create');
+
+$I->seeCurrentUrlEquals('/myorders/create');
+$I->see('Creating a new order');
+$I->seeInField('title', 'DevOps');
+$I->seeInField('payment', 'Eternal gratitude');
+$I->fillField('payment', '10');
 $I->fillField('description', 'Needed: someone who can make photo editor with QTCreator.');
 $I->fillField('deadline', '2021-02-02 00:00');
 $I->click('Create');
 
-$I->seeInDatabase('orders', ['title' => 'DevOps', 'payment' => '100$', 'deadline' => '2021-02-02 00:00']);
+$I->seeInDatabase('orders', ['title' => 'DevOps', 'budget' => '10', 'deadline' => '2021-02-02 00:00']);
 
 
