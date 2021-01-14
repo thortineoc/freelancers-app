4943909221b5cca77aeaf95c60a81e6ac89fd898
Michal Stadryniak <stadryniak@student.agh.edu.pl>
2021-01-14 20:19:27 +0100

    crud methods

--- project/app/Http/Controllers/MyOfferController.php
+++ project/app/Http/Controllers/MyOfferController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Models\Offer;
 use App\Models\Order;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
 
 class MyOfferController extends Controller
@@ -12,23 +14,29 @@ class MyOfferController extends Controller
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function index()
     {
-        echo "MyOfferController index";
+
     }
 
     /**
      * Show the form for creating a new resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return Response
      */
     public function create(Order $order)
     {
         echo "MyOfferController create";
         echo "<br/>";
         echo $order;
+        echo "<br/>";
+        if ($order->user_id == Auth::id())
+        {
+            // set notification string and redirect to order.index?
+            echo "You cant create ofer for your own order";
+        }
         // idk view name
         //return view('myorderoffers.index')->withOrder($order);
     }
@@ -36,9 +44,9 @@ class MyOfferController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param \Illuminate\Http\Request $request
+     * @param Request $request
      * @param Order $order
-     * @return \Illuminate\Http\RedirectResponse
+     * @return RedirectResponse
      */
     public function store(Request $request, Order $order)
     {
@@ -48,7 +56,7 @@ class MyOfferController extends Controller
             'details' => 'required'
         ]);
 
-        $newOffer = new Order();
+        $newOffer = new Offer();
         $newOffer->price =  $request->post('price');
         $newOffer->deadline =  $request->post('deadline');
         $newOffer->details =  $request->post('details');
@@ -64,12 +72,22 @@ class MyOfferController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param Order $order
+     * @param Offer $offer
+     * @return RedirectResponse
      */
     public function show(Order $order, Offer $offer)
     {
         echo "MyOfferController show";
+        echo "<br/>Order:<br/>";
+        echo $order;
+        echo "<br/>Offer:<br/>";
+        echo $offer;
+        if( $offer->user_id != Auth::id())
+        {
+            echo "<br>UNAUTHORIZED<br/>";
+            return redirect()->route('orders');
+        }
 
     }
 
@@ -82,14 +100,20 @@ class MyOfferController extends Controller
      */
     public function edit(Order $order, Offer $offer)
     {
-        echo "MyOfferController edit";
-
+        if( $offer->user_id != Auth::id() )
+        {
+            echo "UNAUTHORIZED";
+            return redirect()->route('orders');
+        }
+        echo $order;
+        // idk view name
+        //return view('offers.edit')->withOffer($offer);
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param \Illuminate\Http\Request $request
+     * @param Request $request
      * @param Order $order
      * @param Offer $offer
      * @return void
@@ -104,10 +128,15 @@ class MyOfferController extends Controller
      *
      * @param Order $order
      * @param Offer $offer
-     * @return void
+     * @return RedirectResponse
      */
     public function destroy(Order $order, Offer $offer)
     {
-        //
+        if( $order->id != $offer->order_id || $offer->user_id != Auth::id() )
+        {
+            return redirect()->route('orders');
+        }
+        $offer->delete();
+        return redirect()->route('orders.offers.index', $book->id);
     }
 }
