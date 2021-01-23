d68f70a6cfb244a635166c53373ae5230f4bbd2c
Mateusz Mulak <mateusz7@icloud.com>
2021-01-23 19:22:28 +0100

    created method to store rating

--- project/app/Http/Controllers/RateController.php
+++ project/app/Http/Controllers/RateController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class RateController extends Controller
@@ -14,6 +15,15 @@ class RateController extends Controller
      */
     public function __invoke(Request $request)
     {
-        //
+        //todo
+        $rate1=5;
+        $rate2=5;
+        $userID=1;
+
+        $user=User::whereId($userID)->first();
+        $user->rate_quality_sum+=$rate1;
+        $user->rate_time_sum+=$rate2;
+        $user->number_of_rates++;
+        $user->save();
     }
 }
