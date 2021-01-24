e9e856293d7da343a36992347382438a8414d4e4
Mateusz Medoń <medonmat1234@gmail.com>
2021-01-24 10:59:09 +0100

    sth

--- project/tests_codeception/acceptance/01_HomepageCept.php
+++ project/tests_codeception/acceptance/01_HomepageCept.php
@@ -8,3 +8,17 @@ $I->seeInTitle('Order Directory For Freelancers');
 
 $I->seeLink('Login', '/login');
 $I->seeLink('Register', '/register');
+
+$I = new AcceptanceTester($scenario);
+$I->amOnPage('/');
+$I->seeLink('Login', '/login');
+$I->click('Login');
+$I->seeCurrentUrlEquals('/login');
+$I->fillField('email', 'john.doe@gmail.com');
+$I->fillField('password', 'secret');
+
+
+/*$I->wantTo('see DF homepage');
+$I->amOnPage('/');
+$I->seeLink('Login', '/login');
+$I->seeLink('Register', '/register');*/
