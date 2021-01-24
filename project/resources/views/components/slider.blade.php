555f4703270c00ddfa593ee162fc6cd3b783319b
Mateusz Medoń <medonmat1234@gmail.com>
2021-01-24 21:52:41 +0100

    slider homepage pr

--- project/resources/views/components/slider.blade.php
+++ project/resources/views/components/slider.blade.php
@@ -1,40 +1,15 @@
-<style>
-    .snap-x {
-        scroll-snap-type: x mandatory;
-
-        scroll-behavior: smooth;
-        -webkit-overflow-scrolling: touch;
-    }
-
-    .snap-start {
-        scroll-snap-align: start;
-    }
-
-    .borders {
-        margin-left: 20%;
-        margin-right: 20%;
-    }
-
-    .center{
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        transform: translate(-50%, -50%);
-    }
-</style>
-
-
+<link rel="stylesheet" href="{{ asset('css/slider.css') }}">
 <div class="flex flex-col items-center m-8 borders bg-color-gray-100" >
 
     <div class="w-full bg-white rounded overflow-x-hidden flex snap-x " style="height: 40vh">
         <div
-            class="snap-start w-full h-full flex items-center justify-center text-white text-4xl font-bold flex-shrink-0  relative fa-align-justify"
+            class="snap-start w-full h-full flex items-center justify-center text-white text-4xl font-bold flex-shrink-0  relative"
             id="slide-1">
-            <h1 class="center">centered</h1>
             <img
                 src="https://cdn.datafloq.com/cache/blog_pictures/878x531/artificial-intelligence-future-of-programming.jpg"
                 class="h-full w-full object-cover absolute">
-
+            <h1 class="z-20 padding text-green-900">Our director of engineering will work with you to understand your goals, technical needs, and team
+                dynamics.</h1>
         </div>
         <div
             class="snap-start w-full h-full flex items-center justify-center text-white text-4xl font-bold flex-shrink-0  relative "
@@ -42,6 +17,8 @@
             <img
                 src="https://www.goodcore.co.uk/blog/wp-content/uploads/2019/08/coding-vs-programming-2.jpg"
                 class="h-full w-full object-cover absolute">
+            <h1 class="z-20 padding text-green-900">Within days, You will find the right developer for your project. Average time to match is under 24
+                hours.</h1>
         </div>
         <div
             class="snap-start w-full h-full flex items-center justify-center text-white text-4xl font-bold flex-shrink-0  relative "
@@ -49,6 +26,7 @@
             <img
                 src="https://www.simplilearn.com/ice9/free_resources_article_thumb/Best-Programming-Languages-to-Start-Learning-Today.jpg"
                 class="h-full w-full object-cover absolute">
+            <h1 class="z-20 padding text-green-900">The largest selection of professionals. Freelancers from all over the world specialized in each programming language.</h1>
         </div>
         <div
             class="snap-start w-full h-full flex items-center justify-center text-white text-4xl font-bold flex-shrink-0  relative "
@@ -56,6 +34,8 @@
             <img
                 src="https://termly.io/wp-content/uploads/2019/11/Privacy-Policy-Template-Featured-Image.png"
                 class="h-full w-full object-cover absolute">
+            <h1 class="z-20 padding text-green-900"> We believe data privacy is one of the most important aspects of business. Our users’ and partners’
+                data is protected in accordance with GDPR.</h1>
         </div>
         <div
             class="snap-start w-full h-full flex items-center justify-center text-white text-4xl font-bold flex-shrink-0  relative "
@@ -63,10 +43,12 @@
             <img
                 src="https://q3p9g6n2.rocketcdn.me/wp-content/ml-loads/2016/09/competition-business-bidding-race-ss-1920.jpg"
                 class="h-full w-full object-cover absolute">
+            <h1 class="z-20 padding text-green-900">  See how they compare to other developers worldwide. Our unique scoring algorithm helps you identify the rising stars.</h1>
         </div>
+
     </div>
 
-    <div class="flex mt-8">
+    <div class="flex mt-10">
         <a class="w-8 mr-1 h-8 text-gray-700 rounded-full bg-white flex justify-center items-center"
            href="#slide-1">1</a>
         <a class="w-8 mr-1 h-8 text-gray-700 rounded-full bg-white flex justify-center items-center"
