a8c4758ec7826aec88416c71a4b1a95130499385
Elżbieta Kubicka <ekubicka@student.agh.edu.pl>
2021-01-26 09:52:20 +0100

    Updated offers cept

--- project/tests_codeception/acceptance/06_OffersCept.php
+++ project/tests_codeception/acceptance/06_OffersCept.php
@@ -3,16 +3,34 @@
 $I = new AcceptanceTester($scenario);
 $I->wantTo('create offers and browse mine');
 
+//Login as John and create order
+
 $I->amOnPage('/orders');
-
 $I->seeCurrentUrlEquals('/login');
-
 $I->fillField('email', 'john.doe@gmail.com');
 $I->fillField('password', 'secret');
 $I->click('Login');
-
 $I->seeCurrentUrlEquals('/orders');
+$I->click('Create new...');
+$I->fillField('title', 'DevOps');
+$I->fillField('budget', 10);
+$I->fillField('description', 'Needed: someone who can make photo editor with QTCreator.');
+$I->fillField('deadline', 'Wed Mar 31 2021');
+$I->click('Create');
 
+//Logout and login as Max
+
+$I->click('Logout');
+$I->seeLink('Login', '/login');
+$I->click('Login');
+$I->seeCurrentUrlEquals('/login');
+$I->fillField('email', 'max.doe@gmail.com');
+$I->fillField('password', 'secret');
+$I->click('Login');
+
+//create offers
+
+$I->amOnPage('/orders');
 $I->seeLink('Apply');
 $I->click('Apply');
 
@@ -33,6 +51,3 @@ $I->amOnPage('/myoffers');
 
 $I->see('There are all of your job offers. When you finish working, please click on the "Finished" button to announce your employer that the job is done. ');
 $I->see('Details about offer');
-$I->click('Finished');
-
-$I->seeCurrentUrlMatches('~/orders/[0-9]+/offer/[0-9]+~');
