ba54e2427ded0c9cced8b3d305505cb1107a3c0f
Elżbieta Kubicka <ekubicka@student.agh.edu.pl>
2021-01-26 22:29:38 +0100

    Added tests for editing order

--- project/tests_codeception/acceptance/05_OrdersCept.php
+++ project/tests_codeception/acceptance/05_OrdersCept.php
@@ -22,27 +22,34 @@ $I->see('Creating a new order');
 $I->click('Create');
 
 $I->seeCurrentUrlEquals('/myorders/create');
-$I->fillField('title', 'DevOps');
+$I->fillField('title', 'Dev Ops');
 $I->click('Create');
 
 $I->seeCurrentUrlEquals('/myorders/create');
 $I->see('Creating a new order');
-$I->seeInField('title', 'DevOps');
+$I->seeInField('title', 'Dev Ops');
 $I->fillField('budget', 0);
 $I->click('Create');
 
 $I->seeCurrentUrlEquals('/myorders/create');
 $I->see('Creating a new order');
-$I->seeInField('title', 'DevOps');
+$I->seeInField('title', 'Dev Ops');
 $I->fillField('budget', 10);
 $I->fillField('description', 'Needed: someone who can make photo editor with QTCreator.');
 $I->fillField('deadline', 'Wed Mar 31 2021');
 $I->click('Create');
 
-$I->seeInDatabase('orders', ['title' => 'DevOps', 'budget' => 10, 'deadline' => 'Wed Mar 31 2021']);
+$I->seeInDatabase('orders', ['title' => 'Dev Ops', 'budget' => 10, 'deadline' => 'Wed Mar 31 2021']);
 
 $I->amOnPage('/myorders');
-$I->see('DevOps');
+$I->see('Dev Ops');
+$I->click('edit');
+$I->seeCurrentUrlMatches('~/myorders/[0-9]+/edit?~');
+$I->fillField('title', 'DevOps');
+$I->fillField('deadline', 'Wed Mar 31 2021');
+$I->click('Update');
+
+$I->seeCurrentUrlEquals('/myorders');
 
 $I->click('See offers...');
 $I->seeCurrentUrlMatches('~/myorders/[0-9]+/offers~');
