72ed002ce38c1c6bded902f524d9848dcf127479
Mateusz Mulak <mateusz7@icloud.com>
2021-01-27 15:40:22 +0100

    update user table

--- project/database/migrations/2014_10_12_000000_create_users_table.php
+++ project/database/migrations/2014_10_12_000000_create_users_table.php
@@ -24,8 +24,8 @@ class CreateUsersTable extends Migration
             $table->double('rate_time_sum')->default(0);
             $table->double('rate_quality_sum')->default(0);
             $table->integer('number_of_rates')->nullable();
-            $table->double('avg_rate_time')->storedAs('rate_time_sum / number_of_rates')->nullable();
-            $table->double('avg_rate_quality')->storedAs('rate_quality_sum / number_of_rates')->nullable();
+            $table->double('avg_rate_time', 3, 2)->storedAs('rate_time_sum / number_of_rates')->nullable();
+            $table->double('avg_rate_quality', 3, 2)->storedAs('rate_quality_sum / number_of_rates')->nullable();
 
         });
     }
